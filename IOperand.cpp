49cc52018e4f8a793fc7f9ad2f88a813465f485e
itsmekate <prasolkaterina@knu.ua>
2019-01-23 19:24:44 +0200

    cleaning files

--- IOperand.cpp
+++
@@ -1,1 +0,0 @@
-#include "IOperand.hpp"
--- Operand.cpp
+++
@@ -1,9 +0,0 @@
-#include "Operand.hpp"
-
-Operand::Operand(eOperandType l, eOperandType r) : left(l), right(r){}
-Operand::~Operand(){}
-
-template <typename Type> type Operand<eOperandType>::operator+(Operand const & rhs)
-{
-  return rhs.left + rhs.right;
-}
